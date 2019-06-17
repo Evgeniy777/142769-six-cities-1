83403b7da2a29c554ef9e7e70449030719de4cce
Evgenii Levashov <evgenii.levashov@gmail.com>
2019-06-18 00:21:45 +0300

    hocs

--- src/components/place-map/place-map.test.js
+++ src/components/place-map/place-map.test.js
@@ -6,37 +6,130 @@ describe(`PlaceMap`, () => {
   it(`PlaceMap correctly renders`, () => {
     const offers = [
       {
-        name: `Beautiful & luxurious apartment at gr eautiful & luxurious apartmenteat location`,
+        city: `Paris`,
+        name: `Beautiful & luxurious apartment at great location`,
         type: `Apartment`,
-        price: 520,
+        price: 120,
         premium: true,
         stars: 93,
         url: `img/apartment-01.jpg`,
-        coordinates: [52.3809533943508, 4.939309666406198]
+        coordinates: [48.85306, 2.34528]
       },
       {
-        name: `Wood Wood and stone place and stone place`,
+        city: `Cologne`,
+        name: `Wood and stone place`,
         type: `Private room`,
-        price: 480,
+        price: 80,
         premium: false,
-        stars: 20,
+        stars: 80,
         url: `img/room.jpg`,
-        coordinates: [52.3909553943508, 4.85309666406198]
+        coordinates: [52.369553943508, 4.85309666406198]
       },
       {
-        name: `Canal View Prinsengracht`,
+        city: `Brussels`,
+        name: `Hotel Novotel Brussels off Grand Place`,
         type: `Apartment`,
-        price: 1672,
-        premium: true,
-        stars: 40,
+        price: 132,
+        premium: false,
+        stars: 80,
         url: `img/apartment-02.jpg`,
-        coordinates: [52.3709553943508, 4.89309666406198]
+        coordinates: [50.846309, 4.355026]
+      },
+      {
+        city: `Brussels`,
+        name: `Hilton Brussels Grand Place`,
+        type: `Apartment`,
+        price: 132,
+        premium: false,
+        stars: 80,
+        url: `img/apartment-02.jpg`,
+        coordinates: [50.845979, 4.356129]
+      },
+      {
+        city: `Amsterdam`,
+        name: `Nice, cozy, warm big bed apartment`,
+        type: `Apartment`,
+        price: 180,
+        premium: true,
+        stars: 100,
+        url: `img/apartment-03.jpg`,
+        coordinates: [52.3809553943508, 4.939309666406198]
+      },
+      {
+        city: `Brussels`,
+        name: `Hotel Novotel Brussels off Grand Place`,
+        type: `Apartment`,
+        price: 132,
+        premium: false,
+        stars: 80,
+        url: `img/apartment-03.jpg`,
+        coordinates: [50.046309, 4.355026]
+      },
+      {
+        city: `Brussels`,
+        name: `Hilton Brussels Grand Place`,
+        type: `Apartment`,
+        price: 132,
+        premium: false,
+        stars: 80,
+        url: `img/room.jpg`,
+        coordinates: [50.245979, 4.256129]
+      },
+      {
+        city: `Hamburg`,
+        name: `Wood and stone place`,
+        type: `Private room`,
+        price: 80,
+        premium: false,
+        stars: 80,
+        url: `img/room.jpg`,
+        coordinates: [53.535, 10.04082]
+      },
+      {
+        city: `Dusseldorf`,
+        name: `Wood and stone place`,
+        type: `Private room`,
+        price: 80,
+        premium: false,
+        stars: 80,
+        url: `img/room.jpg`,
+        coordinates: [52.3809533943508, 4.939309666406198]
       }
     ];
+
+    const cities = [
+      {
+        name: `Paris`,
+        coordinates: [48.864716, 2.349014]
+      },
+      {
+        name: `Cologne`,
+        coordinates: [50.941357, 6.958307]
+      },
+      {
+        name: `Brussels`,
+        coordinates: [50.8505, 4.3488]
+      },
+      {
+        name: `Amsterdam`,
+        coordinates: [52.370216, 4.895168]
+      },
+      {
+        name: `Hamburg`,
+        coordinates: [53.551086, 9.993682]
+      },
+      {
+        name: `Dusseldorf`,
+        coordinates: [51.217941, 6.761680]
+      }
+    ];
+    const city = `Dusseldorf`;
     PlaceMap.prototype.componentDidMount = jest.fn();
     const tree = renderer
       .create(<PlaceMap
         offers={offers}
+        cities={cities}
+        city={city}
       />).toJSON();
     expect(tree).toMatchSnapshot();
   });
